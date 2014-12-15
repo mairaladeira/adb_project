f3149b9e33aa9bab583fac28585b946f82000659
Maira Machado Ladeira <mairamachadoladeira@gmail.com>
2014-12-15 15:06:09 +0100

    BCNF normal form

--- dbnormalizer/core/normalizer/BCNF.py
+++ dbnormalizer/core/normalizer/BCNF.py
@@ -8,3 +8,16 @@ class BCNF(NF):
     def __init__(self):
         super()
         self.nf3 = NF3
+        self.candidate_keys = {'pizza'}
+
+    def is_bcnf(self, fds):
+        for fd in fds:
+            lhs = fds.get_lhs()
+            rhs = fds.get_rhs()
+            is_nf3 = NF3.is_nf3(lhs, rhs)
+            if not is_nf3:
+                return False
+            for f in lhs:
+                if f not in self.candidate_keys:
+                    return False
+        return True
