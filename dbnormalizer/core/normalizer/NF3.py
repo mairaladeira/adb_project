825ef24bfcf38429f47d9dd3044d85ddbeaf943f
Maira Machado Ladeira <mairamachadoladeira@gmail.com>
2014-12-15 13:28:43 +0100

    setup.py changes

--- dbnormalizer/core/normalizer/NF3.py
+++ dbnormalizer/core/normalizer/NF3.py
@@ -18,29 +18,30 @@ class NF3(NF):
     def __init__(self):
         super()
         self.nf2 = NF2
-        print('3rd NF Violation for B->C,D:  ', self.is_3nf('b', {'c', 'd'}));   #3NF violated
-        print('3rd NF Violation for A->B:    ', self.is_3nf('a', {'b'}));   #3NF not violated
+        self.is_nf2 = True
 
     #for 1st functional dependency it will give TRUE as it is in 3NF A->B   A is a PK
-    #for 2nd functional dependency it will give FALSE as it is not in 3NF B->C and B is not PK   (these both are for same table)
+    #for 2nd functional dependency it will give FALSE as it is not in 3NF B->C and B is not PK
+    #(these both are for same table)
             #fdsLHS ='a'
             #fdsRHS ='b'
             #fdsLHS ='b'
             #fdsRHS ={'c','d'}
-    def is_3nf(self, fds_lhs, fds_rhs):
+    def is_nf3(self, fds_lhs, fds_rhs):
         candidate_keys = {'a', 'e', 'f'}
         third_nf_violates = False
         #fdsLHS=functional_dependencies.getLHS_dep();
         #fdsRHS=functional_dependencies.getRHS_dep();
-
-        lhs_is_superkey = False
+        if not self.is_nf2:
+            return False
+        lhs_is_super_key = False
 
         try:
             for e in candidate_keys:
                     if e == fds_lhs:
-                        lhs_is_superkey = True
+                        lhs_is_super_key = True
 
-            if not lhs_is_superkey:
+            if not lhs_is_super_key:
                 for r in fds_rhs:
                     if not is_key_attribute(r):
                         third_nf_violates = True
@@ -50,5 +51,4 @@ class NF3(NF):
             print(ex)
 
         return third_nf_violates
-
 
