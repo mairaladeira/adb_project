2c196faaa5f74b356156415d3eff85584358f9e4
Maira Machado Ladeira <mairamachadoladeira@gmail.com>
2014-12-15 13:06:49 +0100

    setup.py changes

---
+++ dbnormalizer/core/table/__init__.py
@@ -0,0 +1,2 @@
+__author__ = 'Maira'
+#Add here anything that need to be initialized for the table structure
---
+++ dbnormalizer/core/util/__init__.py
@@ -0,0 +1,2 @@
+__author__ = 'Maira'
+#add here anything that needs to be initialized for the util folder
