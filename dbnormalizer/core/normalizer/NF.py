f178efae958fd7143110c716ae55bb4ae5ad33c5
Maira Machado Ladeira <mairamachadoladeira@gmail.com>
2014-12-15 13:34:26 +0100

    setup.py changes

--- dbnormalizer/core/normalizer/NF.py
+++ dbnormalizer/core/normalizer/NF.py
@@ -4,7 +4,15 @@ __author__ = 'Maira'
 class NF:
     def __init__(self):
         self.table = {}
+        self.candidate_keys = {}
 
     #get all the candidates keys for the normal forms
     def get_candidate_keys(self):
-        return ''
+        return self.candidate_keys
+
+    def is_key_attribute(self, attribute):
+        is_key_attr = False
+        for e in self.candidate_keys:
+                if e == attribute:
+                    is_key_attr = True
+        return is_key_attr
