2f0aa7040de6fefc617c231d4f307e1db3f80aee
kaiserco <kaisertimothyco@gmail.com>
2015-01-23 22:46:42 +0100

    minimal cover revision #2
    correcting logic for lhs with multiple attributes. checked against
    example in exam (quiz_mincover.xml in examples folder)

--- dbnormalizer/core/normalizer/NF.py
+++ dbnormalizer/core/normalizer/NF.py
@@ -25,9 +25,6 @@ class NF:
     # @property
     def get_mincover(self, table):
         attrs = [attrib.name for attrib in table.get_attributes]
-        for fd in table.get_fds:
-            fdlhs = [l.name for l in fd.get_lhs]
-            fdrhs = [r.name for r in fd.get_rhs]
         # first step: split rhs
         fd_1 = []
         for fd in table.get_fds:
@@ -36,9 +33,6 @@ class NF:
         step1_table = Table(table.get_name)
         step1_table.set_attributes(table.get_attributes)
         step1_table.set_fds(fd_1)
-        for fd in step1_table.get_fds:
-            fdlhs = [l.name for l in fd.get_lhs]
-            fdrhs = [r.name for r in fd.get_rhs]
         # second step: check for attribute redundancy in lhs
         fd_2 = []
         step2_table = Table(table.get_name)
@@ -52,18 +46,15 @@ class NF:
                     left_closure = []
                     left_closure.append(left)
                     stopper = 0
-                    left_closure_len_curr = 0
                     while stopper == 0:
                         left_closure_len_init = len(left_closure)
                         for fd in step1_table.get_fds:
                             fdlhs = [l.name for l in fd.get_lhs]
-                            fdrhs = [r.name for r in fd.get_rhs]
                             left_names = [n.name for n in left_closure]
                             if set(fdlhs) <= set(left_names):
                                 for right_e in fd.get_rhs:
                                     if not(set([right_e.name]) < set(left_names)):
                                         left_closure.append(right_e)
-                            left_names = [n.name for n in left_closure]
                         if left_closure_len_init == len(left_closure):
                             stopper = 1
                     left_closure_names = [n.name for n in left_closure]
@@ -73,9 +64,6 @@ class NF:
                                 a.remove(left_2)
                 fd_2.append(FD(a,fd1.get_rhs))
         step2_table.set_fds(fd_2)
-        for fd in step2_table.get_fds:
-            fdlhs = [l.name for l in fd.get_lhs]
-            fdrhs = [r.name for r in fd.get_rhs]
         # third step: remove redundant FDs
         fd_3 = []
         step3_table = Table(table.get_name)
@@ -87,22 +75,19 @@ class NF:
             temp.remove(fd1)
             a=fd1.get_lhs
             b=fd1.get_rhs
+            left_closure = []
             for left in a:
-                left_closure = []
                 left_closure.append(left)
             stopper = 0
-            left_closure_len_curr = 0
             while stopper == 0:
                 left_closure_len_init = len(left_closure)
                 for fd in temp:
                     fdlhs = [l.name for l in fd.get_lhs]
-                    fdrhs = [r.name for r in fd.get_rhs]
                     left_names = [n.name for n in left_closure]
                     if set(fdlhs) <= set(left_names):
                         for right_e in fd.get_rhs:
                             if not(set([right_e.name]) <= set(left_names)):
                                 left_closure.append(right_e)
-                    left_names = [n.name for n in left_closure]
                 if left_closure_len_init == len(left_closure):
                     stopper = 1
             left_closure_names = [n.name for n in left_closure]
@@ -113,30 +98,4 @@ class NF:
             if len(c)>0:
                  fd_3.append(FD(a,c))
         step3_table.set_fds(fd_3)
-        for fd in step3_table.get_fds:
-            fdlhs = [l.name for l in fd.get_lhs]
-            fdrhs = [r.name for r in fd.get_rhs]
-        # step 4: combine rhs
-        # fd_4 = []
-        # processed_left = []
-        # step4_table = Table(table.get_name)
-        # step4_table.set_attributes(table.get_attributes)
-        # for fd in step3_table.get_fds:
-        #     for left in fd.get_lhs:
-        #         r = []
-        #         for fd2 in step3_table.get_fds:
-        #             for left2 in fd2.get_lhs:
-        #                 if left.name == left2.name:
-        #                     for right2 in fd2.get_rhs:
-        #                         r.append(right2)
-        #     if not(set(left.name) <= set(processed_left)):
-        #         fd_4.append(FD(fd.get_lhs,r))
-        #         processed_left.append(left.name)
-        # step4_table.set_fds(fd_4)
-        # print("Step 4: FD List")
-        # for fd in step4_table.get_fds:
-        #     fdlhs = [l.name for l in fd.get_lhs]
-        #     fdrhs = [r.name for r in fd.get_rhs]
-        #     # print(fd.get_rhs.name)
-        #     print(fdlhs,"->",fdrhs)
         return fd_3
