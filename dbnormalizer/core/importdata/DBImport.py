f3a2e3dbd66c941b2c460f4dbdad437c982d4c9a
ivastankovic <iva.stankovic0708@gmail.com>
2015-01-24 19:28:26 +0100

    updated schema parameter

--- dbnormalizer/core/importdata/DBImport.py
+++ dbnormalizer/core/importdata/DBImport.py
@@ -3,6 +3,7 @@ __author__ = 'Iva'
 
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.engine import reflection
+from dbnormalizer.core.table import Schema, Table, Attr
 
 # code to run an example
 # username = password = 'adbproject'
@@ -14,16 +15,26 @@ from sqlalchemy.engine import reflection
 class DBImport:
     """ class used to import the db metadata from a postgresql database
     """
-    def __init__(self, username, password, database, schema):
+    def __init__(self, username, password, database, dbschema):
         self.username = username
         self.password = password
         self.database = database
-        self.schema = schema
+        self.schema = dbschema
         self.engine = create_engine('postgresql://' + username + ':' + password + '@localhost/' + database)
         self.connection = self.engine.connect()
         self.metadata = MetaData(self.engine, reflect=True, schema=self.schema)
         self.inspector = reflection.Inspector.from_engine(self.engine)
         self.connection.close()
+
+    def map_tables(self):
+        mapped = Schema(self.schema)
+        for table_name in self.inspector.get_table_names(schema=self.schema):
+            new_table = Table(table_name)
+            columns = self.inspector.get_columns(table_name, self.schema)
+            for c in columns:
+                new_table.add_columns(c['name'])
+            mapped.add_table(Table(table_name))
+        return mapped
 
     def print_table_info(self):
         for table_name in self.inspector.get_table_names(schema=self.schema):
@@ -35,3 +46,5 @@ class DBImport:
             print("\tPrimary key is: ", primary_key['constrained_columns'])
             fk = self.inspector.get_foreign_keys(table_name, self.schema)
             print("\tForeign keys: ", fk)
+
+
