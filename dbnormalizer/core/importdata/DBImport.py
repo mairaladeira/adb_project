c9f04a7466ab26d82886e5a0368c7ca3d521cbe3
ivastankovic <iva.stankovic0708@gmail.com>
2015-01-25 01:32:18 +0100

    added mapping  - schema, attributes, primary keys to dbnormalizer classes

--- dbnormalizer/core/importdata/DBImport.py
+++ dbnormalizer/core/importdata/DBImport.py
@@ -1,16 +1,14 @@
+import dbnormalizer
+from dbnormalizer.core.table.schema import Schema
+from dbnormalizer.core.table.Table import Table
+from dbnormalizer.core.table.Attr import Attribute
+from dbnormalizer.core.table.FD import FD
+
 __author__ = 'Iva'
 
-
-from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.engine import reflection
-from dbnormalizer.core.table import schema, Table, Attr
 
-# code to run an example
-# username = password = 'adbproject'
-# database = 'adb_test'
-# schema = 'test'
-# im = DBImport(username, password, database, schema)
-# im.print_table_info()
 
 class DBImport:
     """ class used to import the db metadata from a postgresql database
@@ -27,14 +25,43 @@ class DBImport:
         self.connection.close()
 
     def map_tables(self):
-        mapped = dbnormalizer.core.table.schema(self.schema)
+        mapped = Schema(self.schema)
         for table_name in self.inspector.get_table_names(schema=self.schema):
             new_table = Table(table_name)
             columns = self.inspector.get_columns(table_name, self.schema)
+            attributes = []
             for c in columns:
-                new_table.add_columns(c['name'])
-            mapped.add_table(Table(table_name))
+                attr_new = Attribute(c['name'])
+                attr_new.set_type(c['type'])
+                attributes.append(attr_new)
+            new_table.set_attributes(attributes)
+            primary_key = self.inspector.get_pk_constraint(table_name, self.schema)
+            fds = self.construct_fd_from_pk(primary_key['constrained_columns'], attributes)
+            new_table.fds = fds
+            mapped.add_table(new_table)
         return mapped
+
+    def construct_fd_from_pk(self, primary, attributes):
+        fds = []
+        prim = []
+        for col in primary:
+            att = Attribute(col)
+            prim.append(att)
+        for att in attributes:
+            same_att = False
+            if isinstance(prim, list):
+                for part in prim:
+                    if part.get_name == att.get_name:
+                        same_att = True
+            elif lhs.get_name == att.get_name:
+                same_att = True
+
+            if not same_att:
+                lhs = prim
+                rhs = att
+                fd = FD(lhs=lhs, rhs=rhs)
+                fds.append(fd)
+        return fds
 
     def print_table_info(self):
         for table_name in self.inspector.get_table_names(schema=self.schema):
@@ -47,4 +74,8 @@ class DBImport:
             fk = self.inspector.get_foreign_keys(table_name, self.schema)
             print("\tForeign keys: ", fk)
 
+
+# proba = DBImport(username='postgres', password='postgres', database='adb_test', dbschema='test')
+# proba.print_table_info()
+# maped = proba.map_tables()
 
