6dcafbd6c1f2324f8b2dca7ca21b54a004c5a51f
Maira Machado Ladeira <mairamachadoladeira@gmail.com>
2015-01-26 04:42:50 +0100

    FD addition feature (edition still in progress)

--- dbnormalizer/web/web.py
+++ dbnormalizer/web/web.py
@@ -7,8 +7,11 @@ from dbnormalizer.core.importdata.XMLImport import XMLImport
 from dbnormalizer.core.importdata.DBImport import DBImport
 import json
 from dbnormalizer.core.util.funcs import get_attributes_list, get_fds_list
+from dbnormalizer.core.table.FD import FD
+
 app = Flask(__name__)
 schema = []
+
 
 def open_browser():
     """
@@ -44,33 +47,48 @@ def upload(button):
     """
     global schema
     print(button)
-
-    if button == "xmlButton":
-        xml_data = request.form["data"]
-        xml_structure = XMLImport(xml_data)
-        xml_structure.init_objects()
-        schema = xml_structure.get_schema()
-        js_object = get_js_object()
-        return js_object
-    elif button == "insertDBButton":
-        url = request.form.get('url')
-        username = request.form.get('user')
-        pwd = request.form.get('pwd')
-        db_name = request.form.get('dbName')
-        schema_name = request.form.get('schema')
-        schema_structure = DBImport(url, username, pwd, schema_name, db_name)
-        if None == schema_structure.get_error():
-            schema = schema_structure.map_tables()
-            js_object = get_js_object()
+    try:
+        if button == "xmlButton":
+            xml_data = request.form["data"]
+            xml_structure = XMLImport(xml_data, False)
+            xml_structure.init_objects()
+            schema = xml_structure.get_schema()
+            js_object = get_display_tables_js_object()
             return js_object
-        else:
-            error = schema_structure.get_error()
-            print(error)
-            return error
-    return "Undefined button: " + button
+        elif button == "insertDBButton":
+            url = request.form.get('url')
+            username = request.form.get('user')
+            pwd = request.form.get('pwd')
+            db_name = request.form.get('dbName')
+            schema_name = request.form.get('schema')
+            schema_structure = DBImport(url, username, pwd, schema_name, db_name)
+            if None == schema_structure.get_error():
+                schema = schema_structure.map_tables()
+                js_object = get_display_tables_js_object()
+                return js_object
+            else:
+                error = schema_structure.get_error()
+                print(error)
+                return error
+        elif button == "requestAddFD":
+            table_name = request.form['table']
+            attrs = schema.get_table_attributes(table_name)
+            js_object = get_add_fds_js_object(attrs)
+            return js_object
+        elif button == "insertFDButton":
+            table_name = request.form['table']
+            lhs = request.form['lhs'].split(',')
+            rhs = request.form['rhs'].split(',')
+            table = schema.get_table_by_name(table_name)
+            fd = FD(lhs, rhs)
+            table.add_fd(fd)
+            return "success"
+        return "Undefined button: " + button
+    except Exception as e:
+        print(str(e))
 
 
-def get_js_object():
+def get_display_tables_js_object():
     tables = schema.get_tables()
     tables_data = []
     for table in tables:
@@ -82,6 +100,13 @@ def get_js_object():
             'fds': fds
         })
     return json.dumps(tables_data)
+
+
+def get_add_fds_js_object(attributes):
+    attr_list = []
+    for attr in attributes:
+        attr_list.append(attr.get_name)
+    return json.dumps(attr_list)
 
 if __name__ == "__main__":
     t = threading.Thread(target=open_browser)
