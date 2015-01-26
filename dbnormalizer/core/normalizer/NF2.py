59ba104eeb844bf2976450cd4badb8fe17ea3687
AlexGattino <campar.aleksandra@gmail.com>
2015-01-26 17:53:35 +0100

    2NF testing

--- dbnormalizer/core/normalizer/NF2.py
+++ dbnormalizer/core/normalizer/NF2.py
@@ -1,3 +1,4 @@
+from dbnormalizer.core.table.FD import FD
 from dbnormalizer.core.normalizer.NF import NF
 from dbnormalizer.core.normalizer.NF1 import NF1
 
@@ -8,3 +9,39 @@ class NF2(NF):
     def __init__(self):
         super()
         self.nf1 = NF1
+        self.is_nf1 = True
+
+    def is_nf2(self, table):
+        fds0=table.get_fds
+        fds = []
+        for fd in fds0:
+            for right_e in fd.rhs:
+                fds.append(FD(fd.get_lhs,[right_e]))
+        if not self.is_nf1:
+            return False
+        candidate_keys = self.candidate_keys[table]
+        ck_names=[]
+        for n in candidate_keys:
+            temp=[a.name for a in n]
+            ck_names.append(temp)
+        try:
+            for fd in fds:
+                lhs = fd.get_lhs()
+                rhs = fd.get_rhs()
+                for r in rhs:
+                    if not self.is_prime_attribute(table,r):
+                        if lhs not in candidate_keys:
+                            lhsn=[l.name for l in lhs]
+                            for c in ck_names:
+                                if set(lhsn)< set(c):
+                                    return False
+            return True
+        except Exception as ex:
+            print('violates_2NF exception')
+            print(ex)
+
+
+
+
+
+
